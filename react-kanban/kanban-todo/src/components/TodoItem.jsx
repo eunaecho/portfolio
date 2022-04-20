687493a39ce4d4112636e04d9409d653fd2e87dc
eunae cho <dmsdo922@gmail.com>
2022-04-20 17:33:34 +0900

    수정 기능 추가

--- react-kanban/kanban-todo/src/components/TodoItem.jsx
+++ react-kanban/kanban-todo/src/components/TodoItem.jsx
@@ -6,6 +6,7 @@ const ItemBlockStyle = ({hover}) => ({
     borderRadius: '10px',
     border: '1px dashed black',
     alignItems: 'center',
+    width: '85%',
     padding: '5px',
     margin: '5px',
     background: 'white',
@@ -27,8 +28,11 @@ const checkCircle = {
 
 const ItemText = {
     flex: 1,
+    border: 'none',
     fontSize : '15px',
     color: '#495458',
+    outline: 'none',
+    width: 'inherit'
 }
 
 const RemoveButton = ({hover}) => ({
@@ -53,14 +57,32 @@ const ModifyButton = ({hover}) => ({
 });
 
 export const TodoItem = (props) => {
+    const [readOnly, setReadOnly] = useState(true);
     const [hover, setHover] = useState(false);
+    const ref = useRef();
     
     function checkItem() {
         console.log('checkItem');
     }
     
-    function callModifyItem() {
-        console.log('callModifyItem');
+    function callModifyItem(e) {
+        // readOnly일 경우
+        if(readOnly){
+            // readOnly 해제
+            setReadOnly(false);
+            //focus input으로
+            ref.current.focus();
+
+            // 수정버튼 -> 저장 버튼으로 바뀌게
+            e.target.innerText='저장'
+        } else {
+            // 수정된 값 전달해서(input 전달x, item 자제를 전달해야함 그래야 index와 value를 받아서 setItem 할 때 편함) 저장하기
+            props.modifyItem(props, ref.current.value);
+
+            // 저장버튼 누르면, 다시 readOnly로
+            setReadOnly(true);
+            e.target.innerText='수정'
+        }
     }
     
     function callRemoveItem() {
@@ -70,7 +92,7 @@ export const TodoItem = (props) => {
     return (
         <div style={ItemBlockStyle({hover})} onPointerOver={()=>setHover(true)} onPointerOut={()=>setHover(false)}>
             <div id='btn-check' style={checkCircle} onClick={checkItem}>{props.index}</div>
-            <div style={ItemText}>{props.content}</div>
+            <input ref={ref} id='input-item' readOnly={readOnly} defaultValue={props.content} style={ItemText}></input>
             <button style={ModifyButton({hover})} onPointerOver={()=>setHover(true)} onPointerOut={()=>setHover(false)} onClick={callModifyItem}>수정</button>
             <button style={RemoveButton({hover})} onPointerOver={()=>setHover(true)} onPointerOut={()=>setHover(false)} onClick={callRemoveItem}>삭제</button>
         </div>
--- react-kanban/kanban-todo/src/components/TodoList.jsx
+++ react-kanban/kanban-todo/src/components/TodoList.jsx
@@ -6,7 +6,7 @@ const listStyle = {
     flex: 1,
     padding: '5px 5px',
     paddingBottom: '20px',
-    overflowY: 'auto',
+    overflowY: 'overLay',
     height: '380px',
 }
 
@@ -20,17 +20,24 @@ const TodoList = () => {
             [...prevItems, data]
         );
     }
+    
+    //삼항 연산자 or useEffect?
+    const modifyItem = (item, mContent) => {
+       setItems( items.map((element, index) => ( 
+           (index===item.index)&&(element===item.content) ? mContent : element)
+        ));
+    }
 
     //filter element로 바꾸니까 바로 삭제됨 
     //https://shin1303.tistory.com/entry/JavaScriptArray-%EB%B0%B0%EC%97%B4Array%EC%97%90%EC%84%9C-forEach-filter-map-reduce-%ED%8A%B9%EC%A7%95%EA%B3%BC-%EC%B0%A8%EC%9D%B4%EC%A0%90
     const removeItem = (i, v) => {
-        setItems(items.filter((element ,index) => !((index===i)&&(element===v))));
+        setItems( items.filter((element ,index) => !((index===i)&&(element===v))));
     }   
     
     return (
         <div id='div-todolist' style={listStyle}>
             <TodoInput addItem={addItem}/>
-            {items.map((v, i) => <TodoItem key={i} index={i} content={v} removeItem={removeItem}/>)}
+            {items.map((v, i) => <TodoItem key={i} index={i} content={v} modifyItem={modifyItem} removeItem={removeItem}/>)}
         </div>
     );
 }
